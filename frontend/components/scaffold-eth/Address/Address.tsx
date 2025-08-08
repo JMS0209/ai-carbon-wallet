20d16c149ff1f1ded5318a7ebf1e29c974c25173
JMS0209 <James_Wei2002@outlook.com>
2025-08-08 19:41:08 +0800

    fixing bugs

--- frontend/components/scaffold-eth/Address/Address.tsx
+++ frontend/components/scaffold-eth/Address/Address.tsx
@@ -1,18 +1,38 @@
 "use client";
 
-import { Address } from "viem";
+import { Address as AddressType, getAddress, isAddress } from "viem";
 
-interface AddressProps {
-  address?: Address;
-  className?: string;
-}
+type AddressProps = {
+  address?: AddressType;
+  disableAddressLink?: boolean;
+  format?: "short" | "long";
+  size?: "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
+  onlyEnsOrAddress?: boolean;
+};
 
-export const AddressComponent = ({ address, className }: AddressProps) => {
-  if (!address) return null;
+export const Address = ({
+  address,
+  disableAddressLink,
+  format,
+  size = "base",
+  onlyEnsOrAddress = false,
+}: AddressProps) => {
+  const checkSumAddress = address ? getAddress(address) : undefined;
+
+  if (!checkSumAddress) {
+    return <span className="text-sm">No address</span>;
+  }
+
+  if (!isAddress(checkSumAddress)) {
+    return <span className="text-error">Wrong address</span>;
+  }
+
+  const shortAddress = checkSumAddress?.slice(0, 6) + "..." + checkSumAddress?.slice(-4);
+  const displayAddress = format === "long" ? checkSumAddress : shortAddress;
 
   return (
-    <span className={`font-mono text-sm ${className}`}>
-      {address.slice(0, 6)}...{address.slice(-4)}
+    <span className={`font-mono text-sm ${size === "lg" ? "text-lg" : size === "xl" ? "text-xl" : "text-sm"}`}>
+      {onlyEnsOrAddress ? displayAddress : displayAddress}
     </span>
   );
 };
